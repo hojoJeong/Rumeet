fa74a7922b8fab3832490d3cf0354d55e8797c69
Sung GwangHyun <gch03944@gmail.com>
2023-03-08 14:08:15 +0900

    [INIT] 프로젝트 초기 세팅 (gradle)

---
+++ src/main/java/com/d204/rumeet/RumeetApplication.java
@@ -0,0 +1,13 @@
+package com.d204.rumeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RumeetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/d204/rumeet/RumeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d204.rumeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
