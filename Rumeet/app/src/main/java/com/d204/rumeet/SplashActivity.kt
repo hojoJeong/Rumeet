c2a56dae2ffa077878d82c33865461b63b0e2fd2
RyuSw-cs <rsw1452@naver.com>
2023-03-14 14:32:50 +0900

    [FEAT] Added gradle properties, hashkey activity

---
+++ Rumeet/app/src/main/java/com/d204/rumeet/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.d204.rumeet
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
