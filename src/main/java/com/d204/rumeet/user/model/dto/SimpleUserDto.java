514411cd9bf63ecc1058b74e24a3e2fabf1214fc
jina <jina.oh.dev@gmail.com>
2023-03-21 18:04:21 +0900

    [FEAT] user 닉네임 검색

--- src/main/java/com/d204/rumeet/user/model/dto/SimpleUserDto.java
+++ src/main/java/com/d204/rumeet/user/model/dto/SimpleUserDto.java
@@ -1,5 +1,13 @@
 package com.d204.rumeet.user.model.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SimpleUserDto {
     int id;
     String email;
