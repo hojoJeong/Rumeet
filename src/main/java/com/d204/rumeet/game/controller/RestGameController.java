435684a32ad0df8462b58466a517ef4305134819
JAY-Winter <rubat0@naver.com>
2023-03-23 00:15:37 +0900

    [FEAT] REST API 를 이용한 Kafka 통신

---
+++ src/main/java/com/d204/rumeet/game/controller/RestGameController.java
@@ -0,0 +1,40 @@
+package com.d204.rumeet.game.controller;
+
+import com.d204.rumeet.game.producer.GameProducer;
+import com.d204.rumeet.game.producer.TopicProducer;
+import org.springframework.web.bind.annotation.*;
+
+
+@RequestMapping("/game")
+@RestController()
+public class RestGameController {
+
+    @PostMapping("/run")
+    @CrossOrigin()
+    public void run(@RequestParam(value = "roomId") int roomId, @RequestParam(value = "userAId") int userAId, @RequestParam(value = "userBId") int userBId) {
+    /*
+        1. 게임을 시작한다
+        - 방 번호, userAId, userBId, 시작시간
+
+        2. 각 유저에 대해서 토픽을 생성하자
+        - <프로젝트-명>.game-id.<gameID>.user-id.<userID>
+        rummet.game-id.1.user-id.1
+
+        3. 각 유저는 상대 유저의 토픽을 추적하자
+     */
+        // 게임에 참여한 인원 관련 토픽 생성
+        String bootstrapServer = "localhost:9092";
+        GameProducer userA = new GameProducer(bootstrapServer, roomId, userAId);
+        GameProducer userB = new GameProducer(bootstrapServer, roomId, userBId);
+        String userATopic = userA.createTopic();
+        String userBTopic = userB.createTopic();
+        TopicProducer topicProducer = new TopicProducer();
+    }
+
+    @PostMapping("/send/message")
+    @CrossOrigin()
+    public void sendMessage(@RequestParam(value = "topic") String topic, @RequestParam(value = "message") String message) {
+        TopicProducer topicProducer = new TopicProducer();
+        topicProducer.sendMessage(topic, message);
+    }
+}
