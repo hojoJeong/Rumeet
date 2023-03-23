c27965bd1797409ef4375d255f07d2f182817af7
JAY-Winter <rubat0@naver.com>
2023-03-23 17:06:34 +0900

    [FIX] kafka bean

--- src/main/java/com/d204/rumeet/game/consumer/GameConsumer.java
+++ src/main/java/com/d204/rumeet/game/consumer/GameConsumer.java
@@ -4,22 +4,22 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 
-@Component
-public class GameConsumer {
-
-    // 특정 토픽을 할당해야함
-    @KafkaListener(topics = "rummet.game-id.3.user-id.2", groupId = "group-id")
-    public void listenGroupA(String message) {
-        System.out.println("userA: " + message);
-        // 매개변수 message 를 A 에게 전달
-        // ...
-    }
-
-    // 특정 토픽을 할당해야함
-    @KafkaListener(topics = "rummet.game-id.3.user-id.1", groupId = "group-id")
-    public void listenGroupB(String message) {
-        System.out.println("userB: " + message);
-        // 매개변수 message 를 B 에게 전달
-        // ...
-    }
-}
+//@Component
+//public class GameConsumer {
+//
+//    // 특정 토픽을 할당해야함
+//    @KafkaListener(topics = "rummet.game-id.3.user-id.2", groupId = "group-id")
+//    public void listenGroupA(String message) {
+//        System.out.println("userA: " + message);
+//        // 매개변수 message 를 A 에게 전달
+//        // ...
+//    }
+//
+//    // 특정 토픽을 할당해야함
+//    @KafkaListener(topics = "rummet.game-id.3.user-id.1", groupId = "group-id")
+//    public void listenGroupB(String message) {
+//        System.out.println("userB: " + message);
+//        // 매개변수 message 를 B 에게 전달
+//        // ...
+//    }
+//}
