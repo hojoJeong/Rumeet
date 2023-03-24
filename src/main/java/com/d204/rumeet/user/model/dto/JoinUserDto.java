d3e43f9f801640806805750dbf8615277258b73e
JAY-Winter <rubat0@naver.com>
2023-03-24 10:18:06 +0900

    [FEAT] 회원가입 DTO user_id

--- src/main/java/com/d204/rumeet/user/model/dto/JoinUserDto.java
+++ src/main/java/com/d204/rumeet/user/model/dto/JoinUserDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class JoinUserDto {
+    int id;
     String email;
     String password;
     String nickname;
--- src/main/java/com/d204/rumeet/user/model/dto/KakaoUserJoinDto.java
+++ src/main/java/com/d204/rumeet/user/model/dto/KakaoUserJoinDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class KakaoUserJoinDto {
+    int id;
     String oauth;
     String profile;
 }
