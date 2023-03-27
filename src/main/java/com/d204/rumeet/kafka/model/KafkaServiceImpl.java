3e61c9a1ca196c9299daa50fb516865fc9dfcf26
성광현 <gch03944@gmail.com>
2023-03-28 01:14:55 +0900

    [FEAT] Kafka 오류 수정

--- src/main/java/com/d204/rumeet/kafka/model/KafkaServiceImpl.java
+++ src/main/java/com/d204/rumeet/kafka/model/KafkaServiceImpl.java
@@ -70,7 +70,7 @@ public class KafkaServiceImpl implements KafkaService {
     @Override
     public Properties setProps() {
         Properties props = new Properties();
-        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("bootstrap.servers", "j8d204.p.ssafy.io:9092");
         props.setProperty("group.id", "group-id");
         props.setProperty("key.deserializer", StringDeserializer.class.getName());
         props.setProperty("value.deserializer", StringDeserializer.class.getName());
