fbba8d87feb7e9332d94ce8e65d5edcd04077904
Ubuntu <ubuntu@ip-172-26-11-47.ap-northeast-2.compute.internal>
2023-03-27 13:10:34 +0000

    [FEAT] fast api dummy data load test

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ app = FastAPI()
 
 @app.get("/load/{mode}/{id}")
 async def rootd(mode, id):
-        #global data_df, pace1_avg, df_1km, df_2km, df_3km, df_5km, pace2_avg, pace3_avg, pace5_avg
+        global data_df, pace1_avg, df_1km, df_2km, df_3km, df_5km, pace2_avg, pace3_avg, pace5_avg
         pace = []
 
-        if (mode == 4):
+        if mode == 4:
             pace_value = pace1_avg.filter(pace1_avg["user_id"] == id) \
                     .select('avg_pace1') \
                     .collect()[0][0]
             pace.append(int(pace_value))
             pace_value.show()
-        elif (mode == 5):
+        elif mode == 5:
             print("mode : 5")
             pace2_avg.show()
             filtered = pace2_avg.filter(pace1_avg["user_id"] == id) \
@@ -24,17 +24,18 @@ async def rootd(mode, id):
                 .collect()[0][0]
             pace.append(int(filtered))
             filtered.show()
-        elif (mode == 6):
+        elif mode == 6:
             filtered = pace3_avg.filter(pace1_avg["user_id"] == id) \
                 .select('avg_pace1', 'avg_pace2', 'avg_pace3') \
                 .collect()[0][0]
             pace.append(int(filtered))
             filtered.show()
-        elif (mode == 7):
+        elif mode == 7:
             print('5km data is empty')
             #df_5km.show()
 
         return {"id": id,
+                "mode": mode,
                 "pace": pace}
 
         # pace_value = pace1_avg.filter(pace1_avg["user_id"] == id) \
