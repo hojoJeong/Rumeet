a8ce50e86a004068c4081e3b2a3c757c398ad61e
Ubuntu <ubuntu@ip-172-26-11-47.ap-northeast-2.compute.internal>
2023-03-27 13:28:00 +0000

    [FEAT] fast api dummy data load test

--- main.py
+++ main.py
@@ -10,34 +10,43 @@ async def rootd(mode, id):
         #global data_df, pace1_avg, df_1km, df_2km, df_3km, df_5km, pace2_avg, pace3_avg, pace5_avg
         pace = []
 
-        if mode == "4":
-            pace1_avg.show()
-            pace_value = pace1_avg.filter(pace1_avg["user_id"] == id) \
-                    .select('avg_pace1') \
-                    .collect()[0][0]
-            pace.append(pace_value)
-            pace_value.show()
-        elif mode =="5":
-            pace2_avg.show()
-            print("mode : 5")
-            filtered = pace2_avg.filter(pace1_avg["user_id"] == id) \
-                .select('avg_pace1', 'avg_pace2') \
+        print("mode:"+type(mode))
+        pace1_avg.show()
+        pace_value = pace1_avg.filter(pace1_avg["user_id"] == id) \
+                .select('avg_pace1') \
                 .collect()[0][0]
-            pace.append(filtered)
-            filtered.show()
-        elif mode == "6":
-            filtered = pace3_avg.filter(pace1_avg["user_id"] == id) \
-                .select('avg_pace1', 'avg_pace2', 'avg_pace3') \
-                .collect()[0][0]
-            pace.append(int(filtered))
-            filtered.show()
-        elif mode == "7":
-            print('5km data is empty')
-            #df_5km.show()
+        pace.append(int(pace_value))
 
-        return {"id": id,
-                "mode": mode,
-                "pace": pace}
+        return {"id": id , "pace":pace}
+
+        # if mode == "4":
+        #     pace1_avg.show()
+        #     pace_value = pace1_avg.filter(pace1_avg["user_id"] == id) \
+        #             .select('avg_pace1') \
+        #             .collect()[0][0]
+        #     pace.append(pace_value)
+        #     pace_value.show()
+        # elif mode =="5":
+        #     pace2_avg.show()
+        #     print("mode : 5")
+        #     filtered = pace2_avg.filter(pace1_avg["user_id"] == id) \
+        #         .select('avg_pace1', 'avg_pace2') \
+        #         .collect()[0][0]
+        #     pace.append(filtered)
+        #     filtered.show()
+        # elif mode == "6":
+        #     filtered = pace3_avg.filter(pace1_avg["user_id"] == id) \
+        #         .select('avg_pace1', 'avg_pace2', 'avg_pace3') \
+        #         .collect()[0][0]
+        #     pace.append(int(filtered))
+        #     filtered.show()
+        # elif mode == "7":
+        #     print('5km data is empty')
+        #     #df_5km.show()
+        #
+        # return {"id": id,
+        #         "mode": mode,
+        #         "pace": pace}
 
         # pace_value = pace1_avg.filter(pace1_avg["user_id"] == id) \
         #         .select('avg_pace1') \
