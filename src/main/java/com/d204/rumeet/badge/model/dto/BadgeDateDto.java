b2baef1bcda5195ea95f8ed9c5c74af1e19b2a07
jina <jina.oh.dev@gmail.com>
2023-03-29 09:30:53 +0900

    [FEAT] 취득한 배지조회 구현

--- src/main/java/com/d204/rumeet/badge/model/dto/BadgeDateDto.java
+++ src/main/java/com/d204/rumeet/badge/model/dto/BadgeDateDto.java
@@ -1,16 +1,14 @@
 package com.d204.rumeet.badge.model.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-
 @AllArgsConstructor
 @Data
-public class BadgeDto {
+public class BadgeDateDto {
     int id;
     String name;
     String description;
     int type;
-
+    long date;
 }
