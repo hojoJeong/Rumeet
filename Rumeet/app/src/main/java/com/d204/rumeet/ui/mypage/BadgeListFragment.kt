c7179b344d6dd9ad12f9e23b8e8978ef1fc2cfe4
hojojeong <jung.hojo54@gmail.com>
2023-03-30 02:23:24 +0900

    [FEAT] 뱃지 리스트 조회 서버 연동 구현 중

--- Rumeet/app/src/main/java/com/d204/rumeet/ui/mypage/BadgeListFragment.kt
+++ Rumeet/app/src/main/java/com/d204/rumeet/ui/mypage/BadgeListFragment.kt
@@ -33,36 +33,36 @@ class BadgeListFragment : BaseFragment<FragmentBadgeListBinding, BaseViewModel>(
         //TODO 뱃지 서버통신
         val myBadge = listOf<BadgeContentListUiModel>(
             BadgeContentListUiModel(
-                "제목",
+                getString(R.string.title_badge_welcome),
                 listOf(
-                    BadgeDetailUiModel(R.drawable.ic_badge_welcome, "웰컴", "2023.03.03", "설명설명"),
-                    BadgeDetailUiModel(R.drawable.ic_badge_welcome, "웰컴", "2023.03.03", "설명설명"),
+                    BadgeDetailUiModel(resources.getStringArray(R.array.url_badge)[], resources.getStringArray(R.array.title_badge), "", "설명설명"),
+                    BadgeDetailUiModel(R.drawable.ic_badge_first_victory_no, "웰컴", "2023.03.03", "설명설명"),
                 ),
             ),
             BadgeContentListUiModel(
-                "제목",
+                getString(R.string.title_badge_distance),
                 listOf(
-                    BadgeDetailUiModel(R.drawable.ic_badge_welcome, "웰컴", "2023.03.03", "설명설명"),
-                    BadgeDetailUiModel(R.drawable.ic_badge_welcome, "웰컴", "2023.03.03", "설명설명"),
-                    BadgeDetailUiModel(R.drawable.ic_badge_welcome, "웰컴", "2023.03.03", "설명설명")
+                    BadgeDetailUiModel(R.drawable.ic_badge_distance_100_no, "웰컴", "2023.03.03", "설명설명"),
+                    BadgeDetailUiModel(R.drawable.ic_badge_distance_500_no, "웰컴", "2023.03.03", "설명설명"),
+                    BadgeDetailUiModel(R.drawable.ic_badge_distance_1000_no, "웰컴", "2023.03.03", "설명설명")
                 )
             ),
             BadgeContentListUiModel(
-                "제목",
+                getString(R.string.title_badge_competition),
                 listOf(
                     BadgeDetailUiModel(R.drawable.ic_badge_welcome, "웰컴", "2023.03.03", "설명설명"),
                     BadgeDetailUiModel(R.drawable.ic_badge_welcome, "웰컴", "2023.03.03", "설명설명"),
                     BadgeDetailUiModel(R.drawable.ic_badge_welcome, "웰컴", "2023.03.03", "설명설명")
                 )            ),
             BadgeContentListUiModel(
-                "제목",
+                getString(R.string.title_badge_team),
                 listOf(
                     BadgeDetailUiModel(R.drawable.ic_badge_welcome, "웰컴", "2023.03.03", "설명설명"),
                     BadgeDetailUiModel(R.drawable.ic_badge_welcome, "웰컴", "2023.03.03", "설명설명"),
                     BadgeDetailUiModel(R.drawable.ic_badge_welcome, "웰컴", "2023.03.03", "설명설명")
                 )            ),
             BadgeContentListUiModel(
-                "제목",
+                getString(R.string.title_badge_relay),
                 listOf(
                     BadgeDetailUiModel(R.drawable.ic_badge_welcome, "웰컴", "2023.03.03", "설명설명"),
                     BadgeDetailUiModel(R.drawable.ic_badge_welcome, "웰컴", "2023.03.03", "설명설명"),
--- Rumeet/app/src/main/java/com/d204/rumeet/ui/mypage/model/BadgeDetailUiModel.kt
+++ Rumeet/app/src/main/java/com/d204/rumeet/ui/mypage/model/BadgeDetailUiModel.kt
@@ -1,7 +1,7 @@
 package com.d204.rumeet.ui.mypage.model
 
 data class BadgeDetailUiModel(
-    val badgeImg: Int,
+    val badgeImg: String,
     val badgeName: String,
     val badgeDate: String,
     val badgeDescription: String
