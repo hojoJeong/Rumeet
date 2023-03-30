136703d241663d25b536c6325a809b364e833c0d
jungeun-park-kr <je991025@gmail.com>
2023-03-30 23:17:07 +0900

    [FEAT] FCM API 수정

--- src/main/java/com/d204/rumeet/fcm/model/service/FcmTokenService.java
+++ src/main/java/com/d204/rumeet/fcm/model/service/FcmTokenService.java
@@ -1,6 +1,7 @@
 package com.d204.rumeet.fcm.model.service;
 
 import com.d204.rumeet.fcm.model.dto.FcmTokenDto;
+import org.springframework.stereotype.Service;
 
 public interface FcmTokenService {
     int updateUserToken(FcmTokenDto fcmTokenDto);
