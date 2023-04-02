0ca4c17ee7c2e143e1cb72d3e27b8339d6a87901
jungeun-park-kr <je991025@gmail.com>
2023-04-02 18:02:04 +0900

    [FEAT] 친구 매칭 수정

--- src/main/java/com/d204/rumeet/game/model/dto/RaceDto.java
+++ src/main/java/com/d204/rumeet/game/model/dto/RaceDto.java
@@ -2,9 +2,11 @@ package com.d204.rumeet.game.model.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RaceDto {
     int id;
     int userId;
