6c5d06024e604f77c395efc960c66312b2962cde
rsw1452@naver.com <rsw1452@gmail.com>
2023-04-09 18:07:39 +0900

    [FEAT] 온보딩 페이지 추가

--- Rumeet/app/src/main/java/com/d204/rumeet/ui/onboarding/OnBoardingFragment.kt
+++ Rumeet/app/src/main/java/com/d204/rumeet/ui/onboarding/OnBoardingFragment.kt
@@ -1,22 +1,23 @@
 package com.d204.rumeet.ui.onboarding
 
-import android.util.Log
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import com.d204.rumeet.R
 import com.d204.rumeet.databinding.FragmentOnboardingBinding
 import com.d204.rumeet.ui.activities.LoginActivity
 import com.d204.rumeet.ui.base.BaseFragment
+import com.d204.rumeet.ui.onboarding.adpater.OnBoardingAdapter
+import com.d204.rumeet.ui.onboarding.model.OnBoardingModel
 import com.d204.rumeet.util.startActivityAfterClearBackStack
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class OnBoardingFragment : BaseFragment<FragmentOnboardingBinding, OnBoardingViewModel>() {
 
     override val layoutResourceId: Int = R.layout.fragment_onboarding
     override val viewModel: OnBoardingViewModel by viewModels()
+    private lateinit var onBoardingAdapter : OnBoardingAdapter
 
     // 뷰모델 실행
     override fun initStartView() {
@@ -44,5 +45,16 @@ class OnBoardingFragment : BaseFragment<FragmentOnboardingBinding, OnBoardingVie
             it.setContent("계속하기")
             it.setState(true)
         }
+        onBoardingAdapter = OnBoardingAdapter(this).apply {
+            setImg(
+                listOf(
+                    OnBoardingModel(R.drawable.bg_onboarding_1, "러닝할 준비 되셨나요?\n나만의 러닝메이트를 찾아보아요!"),
+                    OnBoardingModel(R.drawable.bg_onboarding_2, "나의 체력데이터로\n러닝메이트를 찾아줘요"),
+                    OnBoardingModel(R.drawable.bg_onboarding_3, "혼자서 달리기는 그만!\n경쟁과 협동을 동시에 즐겨요!"),
+                )
+            )
+        }
+        binding.vpOnboardingBanner.adapter = onBoardingAdapter
+        binding.ciOnboardingIndicator.setViewPager(binding.vpOnboardingBanner)
     }
 }
